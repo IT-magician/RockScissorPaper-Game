e6e108bf463e218f7af862a80b294128c0520737
hoan123432 <hoan123432@naver.com>
2023-11-13 21:16:32 +0900

    rewrite: sidebar 'x'버튼 스크롤시 고정으로 수정

--- js/ui.js
+++ js/ui.js
@@ -1,17 +1,7 @@
 // https://www.w3schools.com/howto/howto_js_sidenav.asp
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  let closeBtn = document.querySelector(".sidenav");
 
-  if (closeBtn.classList.contains("opened") === false)
-    closeBtn.classList.add("opened");
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  let closeBtn = document.querySelector(".sidenav");
-
-  if (closeBtn.classList.contains("opened"))
-    closeBtn.classList.remove("opened");
-}
+document.querySelector(".navOpenerBtn").addEventListener("click", e => {
+  const element = e.target
+  document.querySelector(".sidenav").classList.toggle("opened")
+});
